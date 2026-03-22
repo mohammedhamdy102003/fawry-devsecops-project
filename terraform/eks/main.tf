d6d809da038ea14d128062eb6cc447d80d9eac16
Ubuntu <azureuser@web-server.vc1lghznwjuuhh3nklngqtpfmd.parx.internal.cloudapp.net>
2026-03-22 18:06:46 +0000

    edit eks main

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,31 +1,29 @@
 resource "aws_eks_cluster" "main" {
-    name     = "${var.project_name}-eks-cluster"
-    role_arn = var.cluster_role_arn  # اتصلحت هنا
-    version  = "1.29"
+  name     = "${var.project_name}-eks-cluster"
+  role_arn = var.cluster_role_arn
+  version  = "1.29"
 
-    vpc_config {
-      subnet_ids              = var.private_subnet_ids
-      endpoint_private_access = true
-      endpoint_public_access  = true
-    }
+  vpc_config {
+    subnet_ids              = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  depends_on = [var.cluster_policy_attachment]
 }
 
 resource "aws_eks_node_group" "main" {
-    cluster_name  = aws_eks_cluster.main.name
-    node_role_arn = var.node_role_arn
-    subnet_ids    = var.private_subnet_ids
-    
-    node_group_name = "${var.project_name}-node-group" 
-    scaling_config {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-    }
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.project_name}-node-group"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.private_subnet_ids
 
-    instance_types = ["t3.medium"]
-    capacity_type  = "ON_DEMAND"
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
 
-    depends_on = [ 
-      aws_eks_cluster.main 
-    ]
+  instance_types = ["t3.small"]
+  capacity_type  = "SPOT"
 }
