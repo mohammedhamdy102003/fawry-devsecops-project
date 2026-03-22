4af3567d467835cda00a53b46e0045d415c1e579
Ubuntu <azureuser@web-server.vc1lghznwjuuhh3nklngqtpfmd.parx.internal.cloudapp.net>
2026-03-22 17:01:56 +0000

    add tfvars file

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,9 +9,26 @@ module "vpc" {
   availability_zones   = var.availability_zones
 }
 
+
+module "iam" {
+  source       = "./iam"
+  project_name = var.project_name
+}
+
+
+module "eks" {
+  source             = "./eks"
+  project_name       = var.project_name
+
+  private_subnet_ids = module.vpc.private_subnet_ids 
+ 
+  cluster_role_arn   = module.iam.eks_cluster_role_arn
+  node_role_arn      = module.iam.eks_nodes_role_arn
+}
+
+
 module "ecr" {
   source           = "./ecr"
   project_name     = var.project_name
   repository_names = var.repository_names
 }
-
--- terraform/variabels.tf
+++ terraform/variabels.tf
@@ -1,29 +1,8 @@
-variable "repository_names" {
-  description = "List of ECR repository names (e.g., ['frontend', 'backend'])"
-  type        = list(string)
-}
-
-variable "project_name" {
-  description = "Project name for tagging"
-  type        = string
-}
-
-
-variable "vpc_cidr" {
-  type = string
-}
-
-variable "public_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "private_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "availability_zones" {
-  type = list(string)
-}
-variable "cluster_name" {
-  type = string
-}
+variable "project_name" { type = string }
+variable "cluster_name" { type = string }
+variable "vpc_cidr" { type = string }
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+variable "availability_zones" { type = list(string) }
+variable "repository_names" { type = list(string) }
+variable "region" { type = string }
